64751ffd09c5a4923b96bfe406c8606eb6adcf68
ranty <ranty>
2002-10-08 14:23:15 +0000

    Updated to twisted 0.99.2

--- apt_proxy/twisted_compat/compat.py
+++ apt_proxy/twisted_compat/compat.py
@@ -1,19 +1,13 @@
 from twisted import copyright
 
 #This is a gross hack to get non-released features
-if copyright.version in ("0.99.0","0.99.1rc4","0.99.2alpha1"):
+update_needed=0
+if copyright.version in ("0.99.2"):
     print "Updating twisted's process module."
-    import process as new_process
-    from twisted.internet import process
-
-    process.reapProcessHandlers = new_process.reapProcessHandlers
-    process.registerReapProccessHandler = \
-        new_process.registerReapProccessHandler
-    process.unregisterReapProccessHandler = \
-        new_process.unregisterReapProccessHandler
-    process.Process = new_process.Process
-    process.reapProcess = new_process.reapProcess
-
+    if not update_needed:
+        print "No updating required."
 else:
     print "WARNING: apt-proxy has not been tested under this version of"\
           " twisted."
+    if not update_needed:
+        print "WARNING: although it should work without problem."
