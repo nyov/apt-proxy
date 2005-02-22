2a2ada08141c224be9a4f6252323728aec4d0091
haggai <haggai>
2005-02-22 23:59:27 +0000

    * Add Depends to disallow upgrade of python2.3 without upgrade of twisted.  Closes: #294626 * init script: wait longer for apt-proxy to stop, thanks Herbert Thielen for the patch. Closes: #284423 * Version 1.9.26

--- debian/init.d
+++ debian/init.d
@@ -15,6 +15,21 @@ group=nogroup
 test -x $twistd || exit 0
 test -r $application || exit 0
 
+# return true if at least one pid is alive
+function alive()
+{
+    if [ -z "$*" ]; then
+	return 1
+    fi
+    for i in $*; do
+	if kill -0 $i 2> /dev/null; then
+	    return 0
+	fi
+    done
+
+    return 1
+}
+
 
 case "$1" in
     start)
@@ -33,14 +48,28 @@ case "$1" in
 
     stop)
 	echo -n "Stopping apt-proxy"
-	start-stop-daemon --stop --quiet \
-		--pidfile $pidfile
+	count=0
+	pid=$(cat $pidfile 2>/dev/null)
+	while alive $pid; do
+		start-stop-daemon --stop --quiet --pidfile $pidfile
+		count=$(expr $count + 1)
+		if [ $count -gt 20 ]; then
+			break;
+		elif [ $count = 2 ]; then
+			echo -n " [wait $count"
+		elif [ $count -gt 2 ]; then
+			echo -n " $count"
+		fi
+		sleep 1
+	done
+	if [ $count -gt 2 ]; then
+		echo -n "]"
+	fi
 	echo "."	
     ;;
 
     restart)
 	$0 stop
-	sleep 1
 	$0 start
     ;;
     
